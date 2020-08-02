fec9db784d1707bbb96a05dfee3bc075ba1b1a73
croconut <marsteve@ucsc.edu>
2020-08-01 18:08:51 -0700

    init repo
    * going to be using screens., components, globals and static functions so initializing those folders
    * have some example code so i dont forget it in app.tsx, particularly for the important stuff --> can optionally have stuff in props interface OR have required things and children has an interesting requirement as does other property expansion for single component overwriting

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export interface Props {
+  name: string,
+  enthusiasmLevel?: number,
+  children?: React.ReactNode,
+}
+
+//showing property expansion for passing properties
+//children should be react.reactnode and enclosed in a fragment
+//to prevent undef / null / boolean passing
+const App: React.FC<Props> = (props) => {
+  const { name, enthusiasmLevel, children, ...otherProps } = props;
+  return (
+    <View {...otherProps} style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <>{children}</>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
