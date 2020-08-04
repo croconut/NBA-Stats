a933f9a38f6d18b196e034b5f780cf323eb9e505
croconut <marsteve@ucsc.edu>
2020-08-03 17:31:28 -0700

    having typescript specific errors in tests does not cause the test to fail
    * the fix is to only test js files and ensure that the ts/tsx files will compile correctly as part of the pipeline i guess

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,19 +1,17 @@
-import { shallow, ShallowWrapper } from "enzyme";
-import React from "react";
-import App from "../App";
-import { View, StyleSheet, Platform } from "react-native";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var enzyme_1 = require("enzyme");
+var react_1 = require("react");
+var App_1 = require("../App");
 // notable problem: if theres an error popping up on these tsx files
 // there may be no actual error when tests are run => e.g. App component
 // requires a name property to be passed in tsx but isn't passed in the 
 // shallow create below BUT the tests pass anyways :x
-
 // partial solution, require in build for some kind of linting process to
 // check for ts/tsx only errors when parsing all tsx/ts files.
-
-describe("<App />", () => {
-  it("renders correctly", () => {
-    const tree: ShallowWrapper = shallow(<App />);
-    expect(tree).toMatchSnapshot();
-  });
+describe("<App />", function () {
+    it("renders correctly", function () {
+        var tree = enzyme_1.shallow(<App_1.default />);
+        expect(tree).toMatchSnapshot();
+    });
 });
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,24 @@ const { withEnzyme } = require("jest-expo-enzyme");
 module.exports = {
   projects: [
     // Skipping Node because we want to test DOM presets only
-    withEnzyme(require("jest-expo/ios/jest-preset")),
-    withEnzyme(require("jest-expo/android/jest-preset")),
+    // also only testing the compiled js code instead of ts
+    {
+      ...withEnzyme(require("jest-expo/ios/jest-preset")),
+      ...{
+        testMatch: [
+          "**/__tests__/**/*.[j]s?(x)",
+          "**/?(*.)+(spec|test).[j]s?(x)",
+        ],
+      },
+    },
+    {
+      ...withEnzyme(require("jest-expo/android/jest-preset")),
+      ...{
+        testMatch: [
+          "**/__tests__/**/*.[j]s?(x)",
+          "**/?(*.)+(spec|test).[j]s?(x)",
+        ],
+      },
+    },
   ],
 };
