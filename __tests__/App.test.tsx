6f4de19a813ecca723db3525826d5034fc37669f
croconut <marsteve@ucsc.edu>
2020-08-03 17:01:14 -0700

    typescript should be supported with enzyme now

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,11 @@
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import React from "react";
 import App from "../App";
 import { View, StyleSheet, Platform } from "react-native";
 
 describe("<App />", () => {
   it("renders correctly", () => {
-    const tree = shallow(<App />);
+    const tree: ShallowWrapper = shallow(<App />);
     expect(tree).toMatchSnapshot();
   });
 });
